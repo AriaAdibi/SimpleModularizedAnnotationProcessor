74b961d7099f442d5aa5e807b1eeb0330924b591
Aria Adibi <aria.adibi@nakisa.com>
2022-03-07 12:38:05 -0500

    Init:: Simple Modularized Annotation Processing

---
+++ customannotationsandtheirprocessors/processor/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module processor {
+  requires static annotations;
+  requires static com.google.auto.service;
+  requires static java.compiler;
+
+  provides javax.annotation.processing.Processor with processors.BareProcessor;
+
+  exports processors;
+}
